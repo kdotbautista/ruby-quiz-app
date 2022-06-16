0d191df672c581ed46cadd5eee99edbc676a53f1
Kevin <kevvbautista@gmail.com>
2022-06-16 09:34:19 +0800

    test

--- deploy/after_bundle.rb
+++
@@ -1,3 +0,0 @@
-on_app_master do
-  run ". /data/kev_todo/shared/config/env.cloud && cd #{config.release_path}"
-end
--- deploy/after_restart.rb
+++
@@ -1,5 +0,0 @@
-on_app_master do
-  run! ". /data/kev_todo/shared/config/env.cloud && cd #{config.release_path} && bundle exec rake db:migrate"
-
-  run ". /data/kev_todo/shared/config/env.cloud && cd #{config.release_path}"
-end
